043b4638299e2dd514ea95769fb1e7c20130a53b
Ryan Blankenship <ry_blank@hotmail.com>
2019-09-14 15:46:13 -0500

    First Commit

---
+++ main_source/hello_world.py
@@ -0,0 +1,6 @@
+def hello_message():
+    return 'Hello World'
+
+
+if __name__ == '__main__':
+    print(hello_message())
---
+++ test/test_hello_world.py
@@ -0,0 +1,11 @@
+import unittest
+from main_source import hello_world
+
+
+class MyTestCase(unittest.TestCase):
+    def test_hello_message(self):
+        self.assertEqual("Hello World", hello_world.hello_message())
+
+
+if __name__ == '__main__':
+    unittest.main()
